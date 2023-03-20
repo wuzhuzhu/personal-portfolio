ba920f1d96ed139bcc9b7c4e696195d706ed6441
太鼓 <wt183603@alibaba-inc.com>
2023-03-20 18:42:38 +0800

    err test

---
+++ src/pages/blog.tsx
@@ -0,0 +1,61 @@
+import { Suspense } from "react"
+import Link from "next/link"
+import Layout from "src/core/layouts/Layout"
+import { useCurrentUser } from "src/users/hooks/useCurrentUser"
+import logout from "src/auth/mutations/logout"
+import { useMutation } from "@blitzjs/rpc"
+import { Routes, BlitzPage } from "@blitzjs/next"
+import styles from "src/styles/Home.module.css"
+
+/*
+ * This file is just for a pleasant getting started page for your new app.
+ * You can delete everything in here and start from scratch if you like.
+ */
+
+const UserInfo = () => {
+  const currentUser = useCurrentUser()
+  const [logoutMutation] = useMutation(logout)
+
+  if (currentUser) {
+    return (
+      <>
+        <button
+          className={styles.button}
+          onClick={async () => {
+            await logoutMutation()
+          }}
+        >
+          Logout
+        </button>
+        <div>
+          User id: <code>{currentUser.id}</code>
+          <br />
+          User role: <code>{currentUser.role}</code>
+        </div>
+      </>
+    )
+  } else {
+    return (
+      <>
+        <Link href={Routes.SignupPage()} className={styles.button}>
+          <strong>Sign Up</strong>
+        </Link>
+        <Link href={Routes.LoginPage()} className={styles.loginButton}>
+          <strong>Login</strong>
+        </Link>
+      </>
+    )
+  }
+}
+
+const Home: BlitzPage = () => {
+  return (
+    <Layout title="Home">
+      <Suspense fallback="Loading...">
+        <UserInfo />
+      </Suspense>
+    </Layout >
+  )
+}
+
+export default Home
