fc052b237bfd3c3148bf031fd0542f09f935159e
太鼓 <wt183603@alibaba-inc.com>
2023-03-21 14:32:11 +0800

    add questions follow ttr

---
+++ src/pages/questions/[questionId].tsx
@@ -0,0 +1,67 @@
+import { Suspense } from "react";
+import { Routes } from "@blitzjs/next";
+import Head from "next/head";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { useQuery, useMutation } from "@blitzjs/rpc";
+import { useParam } from "@blitzjs/next";
+
+import Layout from "src/core/layouts/Layout";
+import getQuestion from "src/questions/queries/getQuestion";
+import deleteQuestion from "src/questions/mutations/deleteQuestion";
+
+export const Question = () => {
+  const router = useRouter();
+  const questionId = useParam("questionId", "number");
+  const [deleteQuestionMutation] = useMutation(deleteQuestion);
+  const [question] = useQuery(getQuestion, { id: questionId });
+
+  return (
+    <>
+      <Head>
+        <title>Question {question.id}</title>
+      </Head>
+
+      <div>
+        <h1>Question {question.id}</h1>
+        <pre>{JSON.stringify(question, null, 2)}</pre>
+
+        <Link href={Routes.EditQuestionPage({ questionId: question.id })}>
+          Edit
+        </Link>
+
+        <button
+          type="button"
+          onClick={async () => {
+            if (window.confirm("This will be deleted")) {
+              await deleteQuestionMutation({ id: question.id });
+              await router.push(Routes.QuestionsPage());
+            }
+          }}
+          style={{ marginLeft: "0.5rem" }}
+        >
+          Delete
+        </button>
+      </div>
+    </>
+  );
+};
+
+const ShowQuestionPage = () => {
+  return (
+    <div>
+      <p>
+        <Link href={Routes.QuestionsPage()}>Questions</Link>
+      </p>
+
+      <Suspense fallback={<div>Loading...</div>}>
+        <Question />
+      </Suspense>
+    </div>
+  );
+};
+
+ShowQuestionPage.authenticate = true;
+ShowQuestionPage.getLayout = (page) => <Layout>{page}</Layout>;
+
+export default ShowQuestionPage;
