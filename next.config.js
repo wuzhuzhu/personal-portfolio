2d315420034521f9da3ff881ec3367f45b8ae49b
太鼓 <wt183603@alibaba-inc.com>
2023-03-30 18:02:42 +0800

    try responsive img

--- next.config.js
+++ next.config.js
@@ -1,9 +1,23 @@
 // @ts-check
 const { withBlitz } = require("@blitzjs/next")
 
+// todo: rm if not using
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+})
+
 /**
  * @type {import('@blitzjs/next').BlitzConfig}
  **/
-const config = {}
+// todo: rm if not using
+const config = {
+  reactStrictMode: true,
+  images: {
+    domains: ["i.interview.run"],
+    // deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+    // imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+    formats: ["image/avif", "image/webp"],
+  },
+}
 
-module.exports = withBlitz(config)
+module.exports = withBlitz(withBundleAnalyzer(config))
