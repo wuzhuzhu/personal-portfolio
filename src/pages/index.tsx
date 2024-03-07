64872b915537372bf1c1babecab37b8c47976168
wuzhuzhu <walterr@live.cn>
2024-03-07 13:15:29 +0800

    [feat]google adsense

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,30 +1,36 @@
-import { BlitzPage } from "@blitzjs/auth"
-import { Box, Container, HStack, Text, VStack, Image } from "@chakra-ui/react"
-import Layout from "@/components/layout"
-import Hero from "@/components/home/hero"
-import FullCards from "@/components/home/full-cards"
-import ImmersionScroll from "@/components/home/immersion-scroll"
-import Ribbon from "@/components/home/ribbon"
-import Education from "@/components/home/education"
-import { gSP, gSSP } from "@/blitz-server"
-import { Routes } from "@blitzjs/next"
-import getCurrentUser from "@/users/queries/getCurrentUser"
-import Footer from "@/components/home/footer"
-import MailMe from "@/components/home/mail-me"
+import { BlitzPage } from "@blitzjs/auth";
+import { Box, Container, HStack, Text, VStack, Image } from "@chakra-ui/react";
+import Layout from "@/components/layout";
+import Hero from "@/components/home/hero";
+import FullCards from "@/components/home/full-cards";
+import ImmersionScroll from "@/components/home/immersion-scroll";
+import Ribbon from "@/components/home/ribbon";
+import Education from "@/components/home/education";
+import { gSP, gSSP } from "@/blitz-server";
+import { Routes } from "@blitzjs/next";
+import getCurrentUser from "@/users/queries/getCurrentUser";
+import Footer from "@/components/home/footer";
+import MailMe from "@/components/home/mail-me";
+import Script from "next/script";
 
 const Home: BlitzPage = () => {
-  return (
-    <VStack w="full" spacing={{ base: "60px", md: "80px", lg: "100px" }}>
-      <Hero />
-      <Ribbon />
-      <FullCards />
-      <ImmersionScroll />
-      <Education />
-      <MailMe />
-      <Footer />
-    </VStack>
-  )
-}
+	return (
+		<VStack w="full" spacing={{ base: "60px", md: "80px", lg: "100px" }}>
+			<Script
+				async
+				src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7213311370099570"
+				crossorigin="anonymous"
+			/>
+			<Hero />
+			<Ribbon />
+			<FullCards />
+			<ImmersionScroll />
+			<Education />
+			<MailMe />
+			<Footer />
+		</VStack>
+	);
+};
 
 /*
 // Authenticate user before page loads
@@ -65,6 +71,6 @@ const Page: BlitzPage<{ user: TCurrentUser }> = ({ user }) => {
 
 */
 
-Home.getLayout = (page) => <Layout>{page}</Layout>
+Home.getLayout = (page) => <Layout>{page}</Layout>;
 
-export default Home
+export default Home;
